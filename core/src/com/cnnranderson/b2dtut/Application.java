04c401bc1ed2013e6b32a164a1e0147b54ce070c
Conner Anderson <cnnranderson@gmail.com>
2015-03-08 14:47:04 -0700

    Parsing TiledMap Objects Into Box2D Bodies

--- core/src/com/cnnranderson/b2dtut/Application.java
+++ core/src/com/cnnranderson/b2dtut/Application.java
@@ -13,6 +13,7 @@ import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.*;
+import com.cnnranderson.b2dtut.utils.TiledObjectUtil;
 
 import static com.cnnranderson.b2dtut.utils.Constants.PPM;
 
@@ -52,6 +53,8 @@ public class Application extends ApplicationAdapter {
 
         map = new TmxMapLoader().load("Maps/test_map.tmx");
         tmr = new OrthogonalTiledMapRenderer(map);
+
+        TiledObjectUtil.parseTiledObjectLayer(world, map.getLayers().get("collision-layer").getObjects());
     }
 
     @Override
---
+++ core/src/com/cnnranderson/b2dtut/utils/TiledObjectUtil.java
@@ -0,0 +1,40 @@
+package com.cnnranderson.b2dtut.utils;
+
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.objects.PolylineMapObject;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
+
+public class TiledObjectUtil {
+    public static void parseTiledObjectLayer(World world, MapObjects objects) {
+        for(MapObject object : objects) {
+            Shape shape;
+
+            if(object instanceof PolylineMapObject) {
+                shape = createPolyline((PolylineMapObject) object);
+            } else {
+                continue;
+            }
+
+            Body body;
+            BodyDef bdef = new BodyDef();
+            bdef.type = BodyDef.BodyType.StaticBody;
+            body = world.createBody(bdef);
+            body.createFixture(shape, 1.0f);
+            shape.dispose();
+        }
+    }
+
+    private static ChainShape createPolyline(PolylineMapObject polyline) {
+        float[] vertices = polyline.getPolyline().getTransformedVertices();
+        Vector2[] worldVertices = new Vector2[vertices.length / 2];
+
+        for(int i = 0; i < worldVertices.length; i++) {
+            worldVertices[i] = new Vector2(vertices[i * 2] / Constants.PPM, vertices[i * 2 + 1] / Constants.PPM);
+        }
+        ChainShape cs = new ChainShape();
+        cs.createChain(worldVertices);
+        return cs;
+    }
+}
